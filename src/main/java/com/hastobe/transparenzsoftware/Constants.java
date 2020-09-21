b8a86f90de7fe55246895c4d434d11d09503c72e
Marcel Jovic <marcel.jovic@weidmueller.com>
2020-09-21 09:22:52 +0200

    preparations for 1.1.0 release

--- src/main/java/com/hastobe/transparenzsoftware/Constants.java
+++ src/main/java/com/hastobe/transparenzsoftware/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
     public final static String BOUNCY_CASTLE_PROVIDER_CODE = "BC";
     public final static String VERSION = "1.1.0";
-    public final static boolean BETA = true;
+    public final static boolean BETA = false;
     public static final String NAMESPACE_MENNEKES = "http://www.mennekes.de/Mennekes.EdlVerification.xsd";
 }
