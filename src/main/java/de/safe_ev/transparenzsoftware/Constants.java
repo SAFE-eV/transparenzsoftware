31941b254897364e38b0fdee7503b3c26422dddf
Heiko Bobzin <hb@bobz.de>
2024-07-19 07:50:08 +0200

    Changes for OBIS codes and DC compensation

--- src/main/java/de/safe_ev/transparenzsoftware/Constants.java
+++ src/main/java/de/safe_ev/transparenzsoftware/Constants.java
@@ -3,7 +3,7 @@ package de.safe_ev.transparenzsoftware;
 public class Constants {
 
     public final static String BOUNCY_CASTLE_PROVIDER_CODE = "BC";
-    public final static String VERSION = "1.4.0-rc1";
-    public final static boolean BETA = true;
+    public final static String VERSION = "1.4.0";
+    public final static boolean BETA = false;
     public static final String NAMESPACE_MENNEKES = "http://www.mennekes.de/Mennekes.EdlVerification.xsd";
 }
